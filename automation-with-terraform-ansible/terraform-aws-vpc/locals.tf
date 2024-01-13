b638026608358ea3e4ecf70a1f122ad019b16432
NarayanadasuRakesh <126420803+NarayanadasuRakesh@users.noreply.github.com>
2024-01-14 00:46:12 +0530

    Delete automation-with-terraform-ansible directory

--- automation-with-terraform-ansible/terraform-aws-vpc/locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  name = "${var.project_name}-${var.environment}"
-  az_names = slice(data.aws_availability_zones.az.names,0,2)
-}
-
--- automation-with-terraform-ansible/terraform-aws-vpc/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "az" {
-  value = data.aws_availability_zones.az.names
-}
-
-output "allocated_az" {
-  value = slice(data.aws_availability_zones.az.names,0,2)
-}
