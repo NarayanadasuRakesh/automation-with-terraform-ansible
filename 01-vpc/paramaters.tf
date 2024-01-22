49936eb7f26132321920fd90c8d5fed6f10db50b
NarayanadasuRakesh <narayanadasurakesh@gmail.com>
2024-01-22 21:58:37 +0530

    updated: parameters.tf

--- 01-vpc/paramaters.tf
+++ 01-vpc/paramaters.tf
@@ -1,4 +1,4 @@
-resource "aws_ssm_parameter" "vpc_ip" {
+resource "aws_ssm_parameter" "vpc_id" {
   name  = "/${var.project_name}/${var.environment}/vpc_id"
   type  = "String"
   value = module.robotshop.vpc_id
